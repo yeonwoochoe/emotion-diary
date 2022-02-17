4dfbf08c82c3425d0f3de13aefe4a849ebf50607
yeonwoo <chiyy6162@gmail.com>
2022-02-17 21:26:22 +0900

    [feat] page : 페이지별 영역 분리와 useRuducer와 useContext 사용으로  props와 state  정리

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,10 +1,9 @@
-import React from "react";
+import DiaryEditor from "../components/DiaryEditor";
 
 const New = () => {
   return (
     <div>
-      <h1>New</h1>
-      <p>여기는 일기 작성페이지다</p>
+      <DiaryEditor />
     </div>
   );
 };
